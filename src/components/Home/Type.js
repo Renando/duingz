f2ac01f589872002aab45c5203f2d6876fcd1906
DuingZ <58330510+Renando@users.noreply.github.com>
2023-06-02 00:22:08 +0700

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "BISMILLAH",
+          "OPEN JASAr",
+          "PAID EDIT FOTO",
+          "PAID EDIT VIDEO",
         ],
         autoStart: true,
         loop: true,
